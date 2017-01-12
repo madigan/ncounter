2bd827e5c95e3931f2211fcf23bd5f973b35911d
John Lynn <john@silferein.com>
2017-01-11 19:37:22 -0800

    Initialize file layout.

---
+++ app.js
@@ -0,0 +1,12 @@
+function Actor(name, init, hp) {
+	this.name = name;
+	this.init = init;
+	this.hp = hp;
+};
+
+function Encounter() {
+	this.actors = [];
+};
+
+alert(localStorage.getItem('name'));
+
