f04a77a686f8f3b5e812c9112363b859bdcb0b2c
John Lynn <john@silferein.com>
2017-01-11 23:22:48 -0800

    Added basic functionality; still an issue with sorting.

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-function Actor(name, init, hp) {
-	this.name = name;
-	this.init = init;
-	this.hp = hp;
+function Actor(other) {
+	other = other || {};
+	this.name = other.name || "";
+	this.init = other.init || 0;
+	this.hp = other.hp || 0;
+};
+var sort = function(left, right) {
+	if(left.hp==0) {
+		if(right.hp==0) return 0;
+		return 1;
+	} else if(right.hp == 0) {
+		return 1;
+	} else {
+		return left.init==right.init ? 0 : (left.init > right.init ? -1 : 1);
+	}
 };
 
 function Encounter() {
 	this.actors = [];
 };
 
-alert(localStorage.getItem('name'));
+var ViewModel = function() {
+	self = this;
+	self.formActor = ko.observable(new Actor());
+	self.actors = ko.observableArray([]);
+	self.addActor = function() {
+		self.actors.push(new Actor(self.formActor()));
+		self.formActor(new Actor());
+	};
+	self.sortActors = function() {
+		self.actors.sort(sort);
+	};
+	self.endTurn = function() {
+		self.actors.remove(this);
+		self.actors.push(this);
+	};
+};
+ko.applyBindings(new ViewModel());
 
